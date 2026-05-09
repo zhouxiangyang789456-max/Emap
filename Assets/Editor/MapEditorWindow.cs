674eaab23503f1ad6386032e3fe7ee8b1ffd78a3
zhouxiangyang789456-max <zhouxiangyang789456@gmail.com>
2026-05-09 10:17:37 +0800

    Implement terrain resource list editing panel with add/remove functionality

--- Assets/Editor/MapEditorWindow.cs
+++ Assets/Editor/MapEditorWindow.cs
@@ -1,11 +1,13 @@
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MapEditorWindow : EditorWindow
 {
     private int mapWidth = 10;
     private int mapHeight = 10;
     private MapResourceConfig config;
+    private Vector2 scrollPos;
 
     [MenuItem("地图编辑器/打开地图编辑器")]
     static void OpenWindow()
@@ -19,6 +21,8 @@ public class MapEditorWindow : EditorWindow
     {
         GUILayout.Label("地图编辑器", EditorStyles.boldLabel);
 
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
         // 地图参数设置面板
         GUILayout.Label("地图参数", EditorStyles.boldLabel);
         mapWidth = EditorGUILayout.IntField("宽度", mapWidth);
@@ -27,6 +31,46 @@ public class MapEditorWindow : EditorWindow
         // 地形配置
         config = (MapResourceConfig)EditorGUILayout.ObjectField("地形配置", config, typeof(MapResourceConfig), false);
 
-        // 这里添加更多UI元素
+        if (config != null)
+        {
+            // 地形资源列表编辑面板
+            GUILayout.Label("地形资源列表", EditorStyles.boldLabel);
+            if (GUILayout.Button("添加地形"))
+            {
+                AddTerrainType();
+            }
+
+            for (int i = 0; i < config.terrainTypes.Length; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                config.terrainTypes[i].id = EditorGUILayout.IntField("ID", config.terrainTypes[i].id);
+                config.terrainTypes[i].name = EditorGUILayout.TextField("名称", config.terrainTypes[i].name);
+                config.terrainTypes[i].canPass = EditorGUILayout.Toggle("可通过", config.terrainTypes[i].canPass);
+                config.terrainTypes[i].blockUnit = EditorGUILayout.Toggle("阻挡单位", config.terrainTypes[i].blockUnit);
+                config.terrainTypes[i].color = EditorGUILayout.ColorField("颜色", config.terrainTypes[i].color);
+                if (GUILayout.Button("删除"))
+                {
+                    RemoveTerrainType(i);
+                    break; // 避免索引错误
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    void AddTerrainType()
+    {
+        var list = new List<MapResourceConfig.TerrainType>(config.terrainTypes);
+        list.Add(new MapResourceConfig.TerrainType { id = list.Count, name = "新地形", color = Color.green });
+        config.terrainTypes = list.ToArray();
+    }
+
+    void RemoveTerrainType(int index)
+    {
+        var list = new List<MapResourceConfig.TerrainType>(config.terrainTypes);
+        list.RemoveAt(index);
+        config.terrainTypes = list.ToArray();
     }
 }
