bfbefd21a3346d1258f399cb8c1cdad62453a64f
zhouxiangyang789456-max <zhouxiangyang789456@gmail.com>
2026-05-09 10:16:47 +0800

    Add map parameters setting panel to editor window

--- Assets/Editor/MapEditorWindow.cs
+++ Assets/Editor/MapEditorWindow.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class MapEditorWindow : EditorWindow
 {
+    private int mapWidth = 10;
+    private int mapHeight = 10;
+    private MapResourceConfig config;
+
     [MenuItem("地图编辑器/打开地图编辑器")]
     static void OpenWindow()
     {
@@ -14,6 +18,15 @@ public class MapEditorWindow : EditorWindow
     void OnGUI()
     {
         GUILayout.Label("地图编辑器", EditorStyles.boldLabel);
+
+        // 地图参数设置面板
+        GUILayout.Label("地图参数", EditorStyles.boldLabel);
+        mapWidth = EditorGUILayout.IntField("宽度", mapWidth);
+        mapHeight = EditorGUILayout.IntField("高度", mapHeight);
+
+        // 地形配置
+        config = (MapResourceConfig)EditorGUILayout.ObjectField("地形配置", config, typeof(MapResourceConfig), false);
+
         // 这里添加更多UI元素
     }
 }
